b24ec59040e1cc00bad19394c5f8eef62f5482c1
oa2591-png <oa2591@nyu.edu>
2025-10-25 12:40:21 -0500

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -75,7 +75,27 @@ dns_records = {
             86400, #minimum
         ),
     },
-   
+       # New A records
+    'safebank.com.': {
+        dns.rdatatype.A: '192.168.1.102',
+    },
+    'google.com.': {
+        dns.rdatatype.A: '192.168.1.103',
+    },
+    'legitsite.com.': {
+        dns.rdatatype.A: '192.168.1.104',
+    },
+    'yahoo.com.': {
+        dns.rdatatype.A: '192.168.1.105',
+    },
+    'nyu.edu.': {
+        dns.rdatatype.A: '192.168.1.106',
+        dns.rdatatype.TXT: (str(encrypted_value),),  # Encrypted secret data from step 3
+        dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
+        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
+        dns.rdatatype.NS: 'ns1.nyu.edu.',
+    },
+}
     # Add more records as needed (see assignment instructions!
 }
 
